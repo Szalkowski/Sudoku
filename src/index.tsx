5044f959395f8a3725443e586cc0daf33b22b755
Maciej Szałkowski <mac.szalkowski@gmail.com>
2022-11-27 13:15:21 +0100

    add shuffle method

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Card, Content, Grid, Title } from './components'
-import { ThemeProvider } from 'styled-components'
-import { reportWebVitals } from './core'
-import { GlobalStyles, theme } from './styles'
+import {Card, Content, Grid, Title} from 'components'
+import {ThemeProvider} from 'styled-components'
+import {reportWebVitals} from 'core'
+import {GlobalStyles, theme} from 'styles'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <ThemeProvider theme={theme}>
-    <GlobalStyles />
-    <Content data-cy={'content'}>
-      <Title data-cy={'title'}>Sudoku</Title>
-      <Card data-cy={'card'}>
-        <Grid />
-      </Card>
-    </Content>
-  </ThemeProvider>
+    <ThemeProvider theme={theme}>
+        <GlobalStyles/>
+        <Content data-cy={'content'}>
+            <Title data-cy={'title'}>Sudoku</Title>
+            <Card data-cy={'card'}>
+                <Grid/>
+            </Card>
+        </Content>
+    </ThemeProvider>
 )
 
 reportWebVitals()
